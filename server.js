4299581b224c03b07bd84904339f5ce98e793c2d
tony <yahooleo36@gmail.com>
2025-10-19 22:55:20 +0800

    功能d，加上顯示env

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world !");
+app.get("/", (_, res) => {
+  res.send("hello node d !");
+});
+
+app.get("/env", (_, res) => {
+  res.json({
+    appEnv: process.env.APP_ENV,
+    appName: process.env.APP_NAME,
+    apiUrl: process.env.API_URL,
+  });
 });
 
 app.listen("3000");
