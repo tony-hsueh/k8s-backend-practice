252ea2ac7bb6a4a15cfd85618635447c6a987b8f
tony <yahooleo36@gmail.com>
2025-10-25 12:06:16 +0800

    feature a 服務

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello world !");
+  res.send("hello node a");
+});
+
+app.get("/test", (req, res) => {
+  res.json({ message: "hello node a", status: "success" });
+});
+
+app.get("/env", (req, res) => {
+  res.json({
+    API_URL: process.env.API_URL,
+    APP_ENV: process.env.APP_ENV,
+    status: "success",
+  });
 });
 
 app.listen("3000");
