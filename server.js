cfd3a8dac7a866fa27fef5f3b290cea117d28071
Tonyhsueh-807909 <hsuehshao_kang@pchome.tw>
2025-10-30 15:15:48 +0800

    feat: 加上healthy check

--- server.js
+++ server.js
@@ -5,4 +5,19 @@ app.get("/", (req, res) => {
   res.send("hello world !");
 });
 
+// Liveness Probe: /livez
+// 只檢查應用是否存活，不檢查 DB
+app.get("/livez", (req, res) => {
+  res.status(200).send("Alive");
+});
+
+// Readiness Probe: /readyz
+// 檢查應用是否能提供服務，例如 DB 已連線
+app.get("/readyz", (req, res) => {
+  if (!dbConnected) {
+    return res.status(500).send("Not Ready");
+  }
+  res.status(200).send("Ready");
+});
+
 app.listen("3000");
